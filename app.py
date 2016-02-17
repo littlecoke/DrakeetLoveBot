5abd3af32c6b44b31c1607e649038114011541fe
drakeet <drakeet.me@gmail.com>
2016-02-18 00:02:02 +0800

    Merge files.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask
 from flask import render_template, request
 import logging
 import telegram
-from token import token
 
 # from views.todos import todos_view
 
@@ -17,7 +16,9 @@ logging.basicConfig(level=logging.DEBUG,
 # 动态路由
 # app.register_blueprint(todos_view, url_prefix='/todos')
 global bot
-bot = telegram.Bot(token=token)
+# 由于 美国节点，只能git部署，我不得不开源 token，请不要随便拿去用，如需生成你自己的 token
+# 请联系 http://telegram.me/BotFather 谢谢！
+bot = telegram.Bot(token='192666820:AAHLcmxXJ68UvkB-nWgbPVGzb_bkDoTNlcU')
 
 @app.route('/')
 def index():
