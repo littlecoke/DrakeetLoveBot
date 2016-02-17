0ee6f2f627a8f9d6fceccc8f1a5859ff45a8c934
drakeet <drakeet.me@gmail.com>
2016-02-18 00:08:48 +0800

    Small fix.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG,
 # 动态路由
 # app.register_blueprint(todos_view, url_prefix='/todos')
 global bot
-# 由于 美国节点，只能git部署，我不得不开源 token，请不要随便拿去用，如需生成你自己的 token
+# 由于 美国节点，只能 git 部署，我不得不开源 token，请不要随便拿去用，如需生成你自己的 token
 # 请联系 http://telegram.me/BotFather 谢谢！
 bot = telegram.Bot(token='192666820:AAHLcmxXJ68UvkB-nWgbPVGzb_bkDoTNlcU')
 
